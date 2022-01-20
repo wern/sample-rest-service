8f4f6d3d1d4af37f5f9b0e411e623b632a6899fb
Werner Eberling <werner.eberling@mathema.de>
2022-01-20 16:56:55 +0100

    Support opentracing.

--- src/main/java/de/mathema/sample/rest/SampleRestController.java
+++ src/main/java/de/mathema/sample/rest/SampleRestController.java
@@ -2,22 +2,33 @@ package de.mathema.sample.rest;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
+import java.net.http.HttpHeaders;
 import java.util.Date;
+import java.util.Set;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SampleRestController {
+ 
+    private final RestTemplate restTemplate;
+    private String hostname;
 
     private Date notHealthyUntil = new Date(0);
     private Date notReadyUntil = new Date(0);
-    private String hostname;
 
-    public SampleRestController(){
+    public SampleRestController(RestTemplate restTemplate){
+        this.restTemplate=restTemplate;
+
         try {
             hostname = Inet4Address.getLocalHost().getHostName();
         } catch(UnknownHostException ex){
@@ -35,8 +46,18 @@ public class SampleRestController {
     }
 
     @GetMapping("/sampletimeservice")
-    public String sayHelloWithTime(@RequestParam(defaultValue = "John", name="to") String toWhom){
-        return "Hello " + toWhom + " from "+ hostname +"! It's " + new RestTemplate().getForEntity("http://sample-time-service:8080/now", String.class).getBody() + ". :)";
+    public String sayHelloWithTime(@RequestParam(defaultValue = "John", name="to") String toWhom, @RequestHeader MultiValueMap<String,String> headers){
+        Set<String> propagateTheseHeaders = Set.of("x-request-id", 
+                                                    "x-b3-traceid",
+                                                    "x-b3-spanid",
+                                                    "x-b3-parentspanid",
+                                                    "x-b3-sampled",
+                                                    "x-b3-flags",
+                                                    "x-ot-span-context");
+
+        HttpHeaders requestHeaders = HttpHeaders.of(headers, (k,v) -> propagateTheseHeaders.contains(k));
+        final HttpEntity<String> entity = new HttpEntity<String>(new MultiValueMapAdapter<String,String>(requestHeaders.map()));
+        return "Hello " + toWhom + " from "+ hostname +"! It's " + restTemplate.exchange("http://sample-time-service:8080/now", HttpMethod.GET, entity, String.class).getBody() + ". :)";
     }
 
     @GetMapping("/check/ready")
