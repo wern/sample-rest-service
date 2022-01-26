b113fdea1b63684655c6481b4e6d76550542c73c
Werner Eberling <werner.eberling@mathema.de>
2022-01-26 14:43:57 +0100

    Fix jaeger tracing

--- src/main/java/de/mathema/sample/rest/SampleRestController.java
+++ src/main/java/de/mathema/sample/rest/SampleRestController.java
@@ -2,17 +2,10 @@ package de.mathema.sample.rest;
 
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
-import java.net.http.HttpHeaders;
 import java.util.Date;
-import java.util.Set;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.util.MultiValueMap;
-import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -46,18 +39,8 @@ public class SampleRestController {
     }
 
     @GetMapping("/sampletimeservice")
-    public String sayHelloWithTime(@RequestParam(defaultValue = "John", name="to") String toWhom, @RequestHeader MultiValueMap<String,String> headers){
-        Set<String> propagateTheseHeaders = Set.of("x-request-id", 
-                                                    "x-b3-traceid",
-                                                    "x-b3-spanid",
-                                                    "x-b3-parentspanid",
-                                                    "x-b3-sampled",
-                                                    "x-b3-flags",
-                                                    "x-ot-span-context");
-
-        HttpHeaders requestHeaders = HttpHeaders.of(headers, (k,v) -> propagateTheseHeaders.contains(k));
-        final HttpEntity<String> entity = new HttpEntity<String>(new MultiValueMapAdapter<String,String>(requestHeaders.map()));
-        return "Hello " + toWhom + " from "+ hostname +"! It's " + restTemplate.exchange("http://sample-time-service:8080/now", HttpMethod.GET, entity, String.class).getBody() + ". :)";
+    public String sayHelloWithTime(@RequestParam(defaultValue = "John", name="to") String toWhom){
+        return "Hello " + toWhom + " from " + hostname + "! It's " + restTemplate.getForEntity("http://sample-time-service:8080/now", String.class).getBody();
     }
 
     @GetMapping("/check/ready")
--- src/main/java/de/mathema/sample/rest/SampleRestServiceConfiguration.java
+++ src/main/java/de/mathema/sample/rest/SampleRestServiceConfiguration.java
@@ -4,11 +4,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SampleRestServiceConfiguration {
+public class SampleRestServiceConfiguration implements WebMvcConfigurer {
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-    return builder.build();
+        return builder.build();
     }
 }
